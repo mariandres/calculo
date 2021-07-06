4854d1ca2b08b1a1d447313c55e67df9782efb3d
mariandres <86988868+mariandres@users.noreply.github.com>
2021-07-06 17:06:21 -0500

    Update IntegralDefinida.m

--- IntegralDefinida.m
+++ IntegralDefinida.m
@@ -1,8 +1,8 @@
 clear all
 clc
 syms x
-f=input('Ingrese la funcion a integrar ') &2+4x^2-x^5
-a=input('Ingrese el intervalo inferior ') &2
-b=input('Ingrese el intervalo superior ') &8
+f=input('Ingrese la funcion a integrar ') %2+4x^2-x^5
+a=input('Ingrese el intervalo inferior ') %2
+b=input('Ingrese el intervalo superior ') %8
 i=int(f,x,a,b)
 ezplot(f)
