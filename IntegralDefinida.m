2668d6f5c0c0283dcb569d947b39f84f9f9f7c1e
mariandres <86988868+mariandres@users.noreply.github.com>
2021-07-06 17:05:28 -0500

    Update IntegralDefinida.m

--- IntegralDefinida.m
+++ IntegralDefinida.m
@@ -1,7 +1,7 @@
 clear all
 clc
 syms x
-f=input('Ingrese la funcion a integrar ') &2+4^2-x^5
+f=input('Ingrese la funcion a integrar ') &2+4x^2-x^5
 a=input('Ingrese el intervalo inferior ') &2
 b=input('Ingrese el intervalo superior ') &8
 i=int(f,x,a,b)
